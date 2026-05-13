f5d4b56945c10a1423a205fa8b19974e2f655d33
irfanwalele <irfanwalele@gmail.com>
2026-05-12 22:59:13 -0400

    final complete

--- 308.3.1.js
+++ 308.3.1.js
@@ -112,11 +112,47 @@
 // Remember it's a string so put it in quotes
 // It's similar to excel with rows and columns
 // This is confusing, go slow. 
+// There are 4 columns, 5 total rows
 const csv = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
 
-//Loop through the characters of a given CSV string.
-// The .length returns the length of the string object
+// I'll use these for the column header, it's empty, so it should add it after
+let cell1 = "";
+let cell2 = "";
+let cell3 = "";
+let cell4 = "";
+
+//This is a reference to where I am, which column/cell
+let currentCell = 1;
+
+//the standard loop, I understand this
 for (let i = 0; i < csv.length; i++) {
-    const ch = csv[i];
-    console.log(ch);
+  const ch = csv[i]; //it's a character grabber
+
+  if (ch === ",") { //searching for commas
+    // move to the next cell
+    currentCell++;
+  } else if (ch === "\n") { //one it spots this, that means the row is done
+    // end of row: log the 4 cells
+    console.log(cell1, cell2, cell3, cell4);
+
+   
+    cell1 = "";
+    cell2 = "";
+    cell3 = "";
+    cell4 = "";
+    currentCell = 1;
+  } else {
+    
+    if (currentCell === 1) {
+      cell1 = cell1 + ch;
+    } else if (currentCell === 2) {
+      cell2 = cell2 + ch;
+    } else if (currentCell === 3) {
+      cell3 = cell3 + ch;
+    } else if (currentCell === 4) {
+      cell4 = cell4 + ch;
+    }
+  }
 }
+
+// I need to practice this more and talk it out loud, rubber ducky
