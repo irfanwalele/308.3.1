1db73373781831a4cf71beb4ea5af0d31be3a678
irfanwalele <irfanwalele@gmail.com>
2026-05-12 21:24:02 -0400

    first loop through 100

---
+++ 308.3.1.js
@@ -0,0 +1,16 @@
+// https://github.com/irfanwalele/308.3.1
+
+
+// Part 1: Fizz Buzz
+
+//Loop through all numbers from 1 to 100
+//How would I do this? Maybe the for loop
+//Createa a variable called i and start it at 1, so let i = 1
+//i is less than or equal to 100, this way it loops through all of them
+// when i becomes 101, the loop is gonna stop
+//i++ after each run of the loop, increase i by 1
+//then console log i
+let i = 1;
+for (let i = 1; i <= 100; i++) {
+    console.log(i);
+}
